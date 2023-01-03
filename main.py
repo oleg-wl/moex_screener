0c751f63b3fe87e4ea2948ff8670e2df5c574571
Oleg Vl <lordcrabov@yandex.ru>
2023-01-03 15:28:52 +0300

    added: Created class Moex and request method

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import requests
 import json
 
-session = requests.Session()
 
-response = session.get('https://iss.moex.com/iss/engines/stock/markets/bonds/boards/TQCB/securities.json?iss.meta=off').json()
+params = {'group_by':'group',
+          'group_by_filter':'stock_bonds',
+          'iss.meta':'off'}
 
-print(response)
+"""
+Класс для работоы с API Московской биржи. 
+"""
+class Moex():
+    def __init__(self):
+        #Создание объекта сессии 
+        self.session = requests.Session()
+        
+    def request_to_api(self, **query_params):
+        data = self.session.get('https://iss.moex.com/iss/securities.json', params=query_params).json()
+        return data
+    
+r = Moex()
+r1= r.request_to_api(**params)
+
+print(r1)
