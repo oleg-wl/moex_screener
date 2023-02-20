d13facd2a3707a902f6ac1436dcac6f8a48dd881
oleg_vl <oleg_vl@nitro-pc>
2023-02-20 22:10:01 +0300

    (bonds.py) added cbrf rates

--- main.py
+++ main.py
@@ -17,16 +17,22 @@ class Moex():
         self.session = requests.Session()
         
     def request_to_api(self, query_type:str, **query_params):
-        data = self.session.get(f'https://iss.moex.com/iss/{query_type}s.json', params=query_params).json()
+        """
+        Метод для отправки запроса к API Мосбиржи.
+
+        :param str query_type: _description_
+        :return _type_: _description_
+        """
+        data = self.session.get(f'https://iss.moex.com/iss/{query_type}.json', params=query_params).json()
         return data
     
     def get_bonds(self):
         """
-        Экземпляр класса для получения списка торгуемых на Мосбирже облигаций через запрос к API Moex
+        Метод класса для получения списка торгуемых на Мосбирже облигаций через запрос к API Moex
         """               
-        bonds = self.request_to_api('securitie', limit=100, start=0, group_by='group', group_by_filter='stock_bonds', is_trading=1)
+        bonds = self.request_to_api('securities', limit=100, start=0, group_by='group', group_by_filter='stock_bonds', is_trading=1)
         
-        #Цикл для сборки нескольких страниц, выдаваемых запросом к API
+        #Цикл для сборки нескольких страниц, выдаваемых запросом к API с параметром start
         for p in range(1,1000):
             page = self.request_to_api('securitie', limit=100, start=p*100, group_by='group', group_by_filter='stock_bonds', is_trading=1)
             
@@ -34,7 +40,7 @@ class Moex():
                 bonds['securities']['data'].append(value)
             
             if len(page['securities']['data']) < 1:
-                print('Загрузка завершена.')
+                print('Загрузка завершена. Стр:{}'.format(p))
                 break  
         return bonds 
     
@@ -42,7 +48,13 @@ class Moex():
         pass
     
     def get_price(self):
-        pass            
+        pass
+
+    def get_cb_rates(self):
+        #Простой запрос для получения курса ЦБ
+        rates = self.session.get('https://iss.moex.com/iss/statistics/engines/currency/markets/selt/rates.json').json()
+        usd, eur = rates['cbrf']['data'][0][3], rates['cbrf']['data'][0][6]
+
+        return usd, eur
 
 
-   
